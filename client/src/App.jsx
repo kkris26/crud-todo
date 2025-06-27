734beb3e0504e01570f607346942e7f005a900d8
kkris26 <krisnuartha09@gmail.com>
2025-06-27 23:00:29 +0800

    create react app on client

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="flex items-center w-full min-h-screen justify-center">
+        <h1>Hallo</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
