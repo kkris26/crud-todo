742b02396b191e42266d72be0e1af028f45f64a5
kkris26 <krisnuartha09@gmail.com>
2025-07-04 17:34:57 +0800

    remove button icon shadow

--- client/src/components/ButtonIcon.jsx
+++ client/src/components/ButtonIcon.jsx
@@ -4,7 +4,7 @@ const ButtonIcon = ({ children, action }) => {
   return (
     <button
       onClick={action}
-      className=" relative group btn btn-xs btn-square btn-ghost border border-base-200 bg-base-300 shadow"
+      className="  relative group btn btn-xs btn-square btn-ghost border border-base-200 bg-base-300 "
     >
       {children}
     </button>
